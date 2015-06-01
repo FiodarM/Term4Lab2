ce8aae29d6c1a0be5d605aaf0e4c09a488a92f3a
FiodarM <fyodormorozko95@gmail.com>
2015-06-01 21:31:42 +0300

    Release

--- interpolation.py
+++ interpolation.py
@@ -10,23 +10,13 @@ def divdiff(x, y):
     if len(x) == 1:
         return y[0]
 
-    return (divdiff(x[1:], y[1:]) - divdiff(x[:-1], y[:-1])) \
-           / (x[-1] - x[0])
-
-
-def eval_divdiffs(points):
-    X, Y = points
-    divdiffs = np.array([])
-    for i, node in enumerate(X):
-        divdiffs = np.append(divdiffs, divdiff(X[:i + 1], Y[:i + 1]))
-
-    return divdiffs
+    return (divdiff(x[1:], y[1:]) - divdiff(x[:-1], y[:-1])) / (x[-1] - x[0])
 
 
 def newton_p(points):
     X, Y = points
 
-    divdiffs = eval_divdiffs(points)
+    divdiffs = np.array([divdiff(X[:i + 1], Y[:i + 1]) for i, item in enumerate(X)])
 
     def polynom(x):
 
@@ -79,18 +69,16 @@ def tridiag_solve(diags, f):
 
     alpha, beta, gamma = diags
     x = np.zeros_like(f)
-    k, l = np.zeros_like(f), np.zeros_like(f)
+    k, l = np.zeros_like(f[1:]), np.zeros_like(f[1:])
     k[0] = f[0] / beta[0]
     l[0] = - gamma[0] / beta[0]
     for i in range(1, len(f) - 1):
         k[i] = (f[i] - alpha[i]*k[i-1])/(alpha[i]*l[i-1] + beta[i])
         l[i] = - gamma[i] / (alpha[i]*l[i-1] + beta[i])
 
-    x[-1] = (f[-1] - alpha[-1]*k[-2]) / (alpha[-1]*l[-2] + beta[-1])
-    for i, item in enumerate(x[-2:0:-1]):
-        x[-2-i] = (f[-2-i] - alpha[-2-i]*k[-3-i] - gamma[-2-i]*x[-1-i]) /\
-                  (alpha[-2-i]*l[-3-i] + beta[-2-i])
-
-    x[0] = k[0] + l[0] * x[1]
+    x[-1] = (f[-1] - alpha[-1] * k[-1]) / (alpha[-1] * l[-1] + beta[-1])
+    for i in range(0, len(f) - 1)[::-1]:
+        x[i] = k[i] + l[i] * x[i + 1]
 
     return x
+
